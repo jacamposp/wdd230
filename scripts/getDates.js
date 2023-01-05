2f93152d4142d4a56e269ec067e241693bacecd3
Joel Campos <jcamposp10@gmail.com>
2023-01-04 22:48:32 -0600

    creating portal home page

---
+++ scripts/getDates.js
@@ -0,0 +1,15 @@
+var date = new Date();
+
+//set variables
+year = date.getFullYear();
+person = 'Joel Campos Porras';
+country = 'Costa Rica';
+lastModification = 'Last Modification: ' + document.lastModified;
+
+//change HTML
+document.getElementById('year').innerText = year;
+document.getElementById('name').innerText = person;
+document.getElementById('country').innerText = country;
+document.getElementById('lastModified').innerText = lastModification;
+
+
